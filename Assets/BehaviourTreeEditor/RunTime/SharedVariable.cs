4516ddc3c7ed7de38de8d81df25ee0d883791731
murphy <murphyqq1@gmail.com>
2023-12-21 00:30:05 +0800

    feat: overrider SharedVariable abstract methods

---
+++ Assets/BehaviourTreeEditor/RunTime/SharedVariable.cs
@@ -0,0 +1,19 @@
+﻿using BehaviourTreeEditor.SharedVariables;
+
+namespace BehaviourTreeEditor.RunTime
+{
+    public abstract class SharedVariable<T> : SharedVariable
+    {
+        public T sharedValue;
+
+        public override void SetValue(object value)
+        {
+            sharedValue = (T)value;
+        }
+
+        public override object GetValue()
+        {
+            return sharedValue;
+        }
+    }
+}
