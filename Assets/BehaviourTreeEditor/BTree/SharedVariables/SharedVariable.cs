91b964aaf106397dab4adeeec807b08367e43f91
murphy <murphyqq1@gmail.com>
2024-01-23 16:13:34 +0800

    add gitignore

--- Assets/BehaviourTreeEditor/BTree/SharedVariables/SharedVariable.cs
+++ Assets/BehaviourTreeEditor/BTree/SharedVariables/SharedVariable.cs
@@ -4,9 +4,7 @@ namespace BehaviourTreeEditor.SharedVariables
 {
     public abstract class SharedVariable : ScriptableObject
     {
-
         public abstract void SetValue(object value);
-
         public abstract object GetValue();
     }
 }
