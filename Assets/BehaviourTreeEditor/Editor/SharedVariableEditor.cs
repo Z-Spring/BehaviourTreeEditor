ce270c3df3ec42773b8c05843b27b438307bb0c7
murphy <murphyqq1@gmail.com>
2024-01-24 20:58:45 +0800

    refactor the code and add delete button

--- Assets/BehaviourTreeEditor/Editor/SharedVariableEditor.cs
+++ Assets/BehaviourTreeEditor/Editor/SharedVariableEditor.cs
@@ -1,18 +1,30 @@
-﻿using System.CodeDom;
+﻿using System;
 using System.Reflection;
 using BehaviourTreeEditor.BTree.SharedVariables;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Object = UnityEngine.Object;
 
 namespace Editor
 {
     [CustomEditor(typeof(SharedVariable), true)]
     public class SharedVariableEditor : UnityEditor.Editor
     {
-        private FieldInfo[] fields;
+        FieldInfo[] fields;
         SharedVariable myTarget;
         ScriptableObject so;
-        private string label;
+        string label;
+        Texture2D iconTexture;
+        EditorWindow MyEditorWindow => EditorWindow.GetWindow<BTreeEditor>();
+        ScrollView scrollView;
+
+        //todo: why here can use Awake method?
+        private void Awake()
+        {
+            scrollView = MyEditorWindow.rootVisualElement.Q<ScrollView>();
+        }
+
 
         private void OnEnable()
         {
@@ -24,126 +36,121 @@ namespace Editor
 
             myTarget = (SharedVariable)target;
             so = (ScriptableObject)myTarget;
+            iconTexture =
+                EditorGUIUtility.Load("Assets/BehaviourTreeEditor/Gizmos/Delete2.png") as Texture2D;
         }
 
         public override void OnInspectorGUI()
         {
-            foreach (var field in fields)
+            if (so == null || myTarget == null)
             {
-                label = $"{so.name} ({field.FieldType.Name})";
-
-                if (so != null)
-                {
-                    if (field.FieldType == typeof(bool))
-                    {
-                        bool currentValue = (bool)field.GetValue(myTarget);
-                        bool newValue = EditorGUILayout.Toggle(label, currentValue);
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(string))
-                    {
-                        string newValue = EditorGUILayout.TextField(label,
-                            field.GetValue(myTarget)?.ToString());
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(int))
-                    {
-                        int currentValue = (int)field.GetValue(myTarget);
-                        int newValue = EditorGUILayout.IntField(label, currentValue);
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(float))
-                    {
-                        float currentValue = (float)field.GetValue(myTarget);
-                        float newValue = EditorGUILayout.FloatField(label, currentValue);
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(Vector2))
-                    {
-                        Vector2 currentValue = (Vector2)field.GetValue(myTarget);
-                        Vector2 newValue = EditorGUILayout.Vector2Field(label, currentValue);
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(Vector3))
-                    {
-                        Vector3 currentValue = (Vector3)field.GetValue(myTarget);
-                        Vector3 newValue = EditorGUILayout.Vector3Field(label, currentValue);
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(Color))
-                    {
-                        Color currentValue = (Color)field.GetValue(myTarget);
-                        Color newValue = EditorGUILayout.ColorField(label, currentValue);
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(AnimationCurve))
-                    {
-                        AnimationCurve currentValue = (AnimationCurve)field.GetValue(myTarget);
-                        AnimationCurve newValue = EditorGUILayout.CurveField(label, currentValue);
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(Bounds))
-                    {
-                        Bounds currentValue = (Bounds)field.GetValue(myTarget);
-                        Bounds newValue = EditorGUILayout.BoundsField(label, currentValue);
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(Rect))
-                    {
-                        Rect currentValue = (Rect)field.GetValue(myTarget);
-                        Rect newValue = EditorGUILayout.RectField(label, currentValue);
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(Quaternion))
-                    {
-                        Quaternion currentValue = (Quaternion)field.GetValue(myTarget);
-                        Quaternion newValue =
-                            Quaternion.Euler(EditorGUILayout.Vector3Field(label, currentValue.eulerAngles));
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(GameObject))
-                    {
-                        GameObject currentValue = (GameObject)field.GetValue(myTarget);
-                        GameObject newValue =
-                            EditorGUILayout.ObjectField(label, currentValue, typeof(GameObject), true) as GameObject;
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(Transform))
-                    {
-                        Transform currentValue = (Transform)field.GetValue(myTarget);
-                        Transform newValue =
-                            EditorGUILayout.ObjectField(label, currentValue, typeof(Transform), true) as Transform;
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(Texture2D))
-                    {
-                        Texture2D currentValue = (Texture2D)field.GetValue(myTarget);
-                        Texture2D newValue =
-                            EditorGUILayout.ObjectField(label, currentValue, typeof(Texture2D), true) as Texture2D;
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(Sprite))
-                    {
-                        Sprite currentValue = (Sprite)field.GetValue(myTarget);
-                        Sprite newValue =
-                            EditorGUILayout.ObjectField(label, currentValue, typeof(Sprite), true) as Sprite;
-                        field.SetValue(myTarget, newValue);
-                    }
-                    else if (field.FieldType == typeof(Material))
-                    {
-                        Material currentValue = (Material)field.GetValue(myTarget);
-                        Material newValue =
-                            EditorGUILayout.ObjectField(label, currentValue, typeof(Material), true) as Material;
-                        field.SetValue(myTarget, newValue);
-                    }
-                }
-
-                // 获取字段的值
-                // 对于 bool 型字段
+                return;
             }
 
-            // Save changes to the SharedVariable
+            var field = fields[0];
+
+            label = $"{so.name} ({field.FieldType.Name})";
+            EditorGUILayout.BeginHorizontal();
+
+            object currentValue = field.GetValue(myTarget);
+            object newValue = null;
+
+            switch (field.FieldType.Name)
+            {
+                case "Boolean":
+                    newValue = EditorGUILayout.Toggle(label, (bool)currentValue);
+                    break;
+                case "Int32":
+                    newValue = EditorGUILayout.IntField(label, (int)currentValue);
+                    break;
+                case "String":
+                    newValue = EditorGUILayout.TextField(label, (string)currentValue);
+                    break;
+                case "Single":
+                    newValue = EditorGUILayout.FloatField(label, (float)currentValue);
+                    break;
+
+                case "Vector2":
+                    newValue = EditorGUILayout.Vector2Field(label, (Vector2)currentValue);
+                    break;
+                case "Vector3":
+                    newValue = EditorGUILayout.Vector3Field(label, (Vector3)currentValue);
+                    break;
+                case "Quaternion":
+                    newValue = EditorGUILayout.Vector3Field(label, ((Quaternion)currentValue).eulerAngles);
+                    break;
+
+                case "Color":
+                    newValue = EditorGUILayout.ColorField(label, (Color)currentValue);
+                    break;
+                case "Rect":
+                    newValue = EditorGUILayout.RectField(label, (Rect)currentValue);
+                    break;
+                case "GameObject":
+                    newValue = EditorGUILayout.ObjectField(label, (GameObject)currentValue, typeof(GameObject),
+                        true);
+                    break;
+                case "Transform":
+                    newValue = EditorGUILayout.ObjectField(label, (Transform)currentValue, typeof(Transform), true);
+                    break;
+                case "Texture2D":
+                    newValue = EditorGUILayout.ObjectField(label, (Texture2D)currentValue, typeof(Texture2D), true);
+                    break;
+                case "Sprite":
+                    newValue = EditorGUILayout.ObjectField(label, (Sprite)currentValue, typeof(Sprite), true);
+                    break;
+                case "Material":
+                    newValue = EditorGUILayout.ObjectField(label, (Material)currentValue, typeof(Material), true);
+                    break;
+                case "AnimationCurve":
+                    newValue = EditorGUILayout.CurveField(label, (AnimationCurve)currentValue);
+                    break;
+                case "Object":
+                    newValue = EditorGUILayout.ObjectField(label, (Object)currentValue, typeof(Object), true);
+                    break;
+                case "Collider":
+                    newValue = EditorGUILayout.ObjectField(label, (Collider)currentValue, typeof(Collider), true);
+                    break;
+                case "LayerMask":
+                    newValue = EditorGUILayout.LayerField(label, (LayerMask)currentValue);
+                    break;
+                case "Bounds":
+                    newValue = EditorGUILayout.BoundsField(label, (Bounds)currentValue);
+                    break;
+            }
+
+            field.SetValue(myTarget, newValue);
             EditorUtility.SetDirty(target);
+            var buttonGUIContent = new GUIContent(iconTexture);
+            if (GUILayout.Button(buttonGUIContent, GUILayout.Width(20), GUILayout.Height(18)))
+            {
+                // newValue = string.Empty;
+                if (EditorUtility.DisplayDialog("Delete variable",
+                        "Are you sure you want to delete this variable?", "Yes", "No"))
+                {
+                    DestroyImmediate(so, true);
+
+                    Delete(myTarget);
+                }
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        void Delete(SharedVariable variable)
+        {
+            if (CreateSharedVariableEditor.dic.TryGetValue(variable, out var container))
+            {
+                CreateSharedVariableEditor.dic.Remove(variable);
+                if (scrollView.Contains(container))
+                {
+                    scrollView.Remove(container);
+                }
+
+                SharedVariablePopupEditor.RemoveSharedVariable(variable);
+            }
+
+            DestroyImmediate(variable);
         }
     }
 }
